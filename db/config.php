f7f324b631cc29383ec0d460bf8c1bbc5e7b1bd2
dmkzera <Daniel-rochacba@hotmail.com>
2025-06-27 19:15:32 -0400

    Update config.php

--- db/config.php
+++ db/config.php
@@ -1,14 +1,11 @@
 <?php
-// Configuração do Banco de Dados
 define('DB_SERVER', '127.0.0.1');
-define('DB_USERNAME', 'root'); // Altere para seu usuário
-define('DB_PASSWORD', ''); // Altere para sua senha
+define('DB_USERNAME', 'root'); 
+define('DB_PASSWORD', ''); 
 define('DB_NAME', 'kanban_db');
 
-// Tenta conectar ao banco de dados MySQL
 $conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
-// Checa a conexão
 if($conn->connect_error){
     header('Content-Type: application/json');
     http_response_code(500);
@@ -16,7 +13,6 @@ if($conn->connect_error){
     die();
 }
 
-// Define o charset para UTF-8 para suportar caracteres especiais
 $conn->set_charset("utf8mb4");
 
 ?>
