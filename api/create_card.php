6658da57ecbf34317ac0880a1c4a67a27b411fb1
dmkzera <Daniel-rochacba@hotmail.com>
2025-06-27 19:17:16 -0400

    Update create_card.php

--- api/create_card.php
+++ api/create_card.php
@@ -3,12 +3,12 @@ require_once '../db/config.php';
 
 header('Content-Type: application/json');
 
-// Pega os dados enviados via POST
+
 $data = json_decode(file_get_contents('php://input'), true);
 
 $title = $data['title'] ?? '';
 $description = $data['description'] ?? '';
-$status = 'todo'; // Novos cards sempre começam em "A Fazer"
+$status = 'todo'; 
 
 if (!empty($title)) {
         $stmt = $conn->prepare("INSERT INTO cards (title, description, status, production_steps, lamination_details) VALUES (?, ?, ?, '{}', '{}')");
